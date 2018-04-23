886923ade73e87a9978f91a32eae87595e59d6a4
Moe yajima <yajima_moe@nexway.co.jp>
2018-04-23 04:55:16 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'intense-refuge-42729.herokuapp.com' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
 end
